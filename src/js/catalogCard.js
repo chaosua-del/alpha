cd1dabccb488d9178825a11a57da6eeeb6f5a759
chaosua-del <d.yuhymenko@gmail.com>
2020-11-12 17:02:33 +0100

    feat: catalogCard page

--- src/js/catalogCard.js
+++ src/js/catalogCard.js
@@ -14,7 +14,33 @@ $(document).ready(function () {
       swiper: galleryThumbs
     }
   });
- 
+
+  // counter
+  const plus = $('.info__plus');
+  const minus = $('.info__minus')
+  const value = $('.info__counter-number');
+  let i = 1;
+
+  plus.on('click', () => {
+    i++;
+    if(i>9)
+    value.text(`${i}`);
+    else
+    value.text(`0${i}`);
+  });
+
+  minus.on('click', () => {
+    if(i > 1 && i < 10) {
+      i--;
+      value.text(`0${i}`);
+    }
+    else if(i > 9) {
+      i--;
+      value.text(`${i}`);
+    }
+    else
+    return;
+  });
     
   });
   
